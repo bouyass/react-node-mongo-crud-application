43697ce0f6685b520353844efeb7e1fcb540fbe5
lyes makhloufi <lyes.makhloufi@outlook.fr>
2020-11-04 01:42:37 +0100

    committing filters implementation

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,11 +14,23 @@ function Home() {
     const [employees, setEmployees] = useState([])
     const [searchWord, setSearchWord] = useState('')
     const [filteredEmployees, setFilteredEmployees] = useState([])
+    const [filteredEmployeesHistoric, setFilteredEmployeesHistoric] = useState([])
     const [shownEmployees, setShownEmployees] = useState([])
     const [nbrItems, setNbrItems] = useState(10)
     const [currentPage, setCurrentPage] = useState(0)
     const [pages, setPages] = useState([])
+    const [minSalaryFilter, setMinSalaryFilter] = useState(0)
+    const [maxSalaryFilter, setMaxSalaryFilter] = useState(0)
+    const [filter, setFilter] = useState(false)
 
+
+    const minSalaryFilterChange = (e) => {
+        setMinSalaryFilter(e.target.value)
+    }
+
+    const maxSalaryFilterChange = (e) => {
+        setMaxSalaryFilter(e.target.value)
+    }
 
     const AddEmployee = () => {
         window.location.replace('/add')
@@ -67,11 +79,19 @@ function Home() {
         }
     }
 
+    const inputMinSalaryFilter = (e) => {
+        setMinSalaryFilter(e.target.value)
+    }
+
+    const inputMaxSalaryFilter = (e) => {
+        setMaxSalaryFilter(e.target.value)
+    }
+
     
     const searchWordChange = (e) => {
         setSearchWord(e.target.value)
         setFilteredEmployees([])
-        var employeeTempArray = []
+        let employeeTempArray = []
         employees.map(employee => {
             var valuesTempArray = Object.values(employee).slice(1,11)
             for(var i = 0; i < valuesTempArray.length; i++){
@@ -101,7 +121,66 @@ function Home() {
         }else {
                 setCurrentPage(0)
                 setPages([0])
+        }
+    }
+
+    const applyFilters = (e) => {
+        setFilter(true)
+        setFilteredEmployeesHistoric(filteredEmployees)
+        setFilteredEmployees([])
+        var employeeTempArray = []
+        employees.map(employee => {
+            if(Number(employee.salary) >= Number(minSalaryFilter) && Number(employee.salary) <= Number(maxSalaryFilter)){
+                employeeTempArray.push(employee)
             }
+        })
+        console.log(employeeTempArray)
+        setFilteredEmployees(employeeTempArray)
+        setShownEmployees(employeeTempArray.slice(0,nbrItems))
+
+        if(employeeTempArray.length > nbrItems){
+            if(employeeTempArray.length%nbrItems > 0){
+                for(let i = 0; i < ((employeeTempArray.length/nbrItems));i++){
+                    pages_temp.push(i)
+                }
+                setPages(pages_temp)
+            }else{
+                for(let i = 0; i < (employeeTempArray.length/nbrItems);i++){
+                    pages_temp.push(i)
+                    
+                }
+               
+                setPages(pages_temp)
+            }
+        }else {
+                setCurrentPage(0)
+                setPages([0])
+        }
+    }
+
+    const desableFilters = (e) => {
+        setFilteredEmployees(filteredEmployeesHistoric)
+        setShownEmployees(filteredEmployeesHistoric.slice(0,nbrItems))
+
+        if(filteredEmployeesHistoric.length > nbrItems){
+            if(filteredEmployeesHistoric.length%nbrItems > 0){
+                for(let i = 0; i < ((filteredEmployeesHistoric.length/nbrItems));i++){
+                    pages_temp.push(i)
+                }
+                setPages(pages_temp)
+            }else{
+                for(let i = 0; i < (filteredEmployeesHistoric.length/nbrItems);i++){
+                    pages_temp.push(i)
+                    
+                }
+               
+                setPages(pages_temp)
+            }
+        }else {
+                setCurrentPage(0)
+                setPages([0])
+        }
+        setFilteredEmployeesHistoric([])
     }
 
     
@@ -180,10 +259,26 @@ function Home() {
                 <Modal.Header closeButton>
                     <Modal.Title>   Filter settings    </Modal.Title>
                 </Modal.Header>
-                <Modal.Body>    Filters here    </Modal.Body>
+                <Modal.Body>  
+                    <div className="filter-container"> 
+                    <h3>Employee salary</h3> 
+                    
+                    <div className="minSalaryFilter">
+                    <label for="customRange1">Min salary</label>
+                    <input type="range" min="0" max="200000" value={minSalaryFilter} step="5" onChange={minSalaryFilterChange} class="slider" id="myRange" />
+                    <span> <input onChange={inputMinSalaryFilter} id="minSalaryInput" type="number" value={minSalaryFilter} /> &nbsp; <b>$</b></span>
+                    </div>
+                    <div className="maxSalaryFilter">
+                    <label for="customRange1">Max salary</label>
+                    <input type="range" min="0" max="200000" value={maxSalaryFilter} step="5" onChange={maxSalaryFilterChange} class="slider" id="myRange" />
+                    <span> <input onChange={inputMaxSalaryFilter} id="maxSalaryInput" type="number" value={maxSalaryFilter} /> &nbsp; <b>$</b></span>
+                    </div>
+                    </div>
+                </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={() => setFilterModal(false)}>Close</Button>
-                    <Button variant="primary">Apply filters</Button>
+                    <Button onClick={applyFilters} variant="primary">Apply filters</Button>
+                    <Button onClick={desableFilters} variant="primary">Desable filters</Button>
                 </Modal.Footer>
             </Modal>
         </div>
